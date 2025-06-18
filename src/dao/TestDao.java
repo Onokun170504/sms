89ed291d2aa2cd2fdf5e7cc1b1861588792f9856
hikatyan <ymg2465052@stu.o-hara.ac.jp>
2025-06-18 12:22:56 +0900

    meippai

--- src/dao/TestDao.java
+++ src/dao/TestDao.java
@@ -3,54 +3,114 @@ package dao;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
 
 import javax.security.auth.Subject;
 
 import bean.School;
+import bean.Student;
 import bean.Test;
 
+
+
 public class TestDao extends Dao {
-    
-    public List<Test> filter(Subject subject, School school) throws Exception {
+
+	public List<Test> get(Student student, Subject subject, School school) throws Exception {
+	    List<Test> list = new ArrayList<>();
+	    Connection connection = getConnection();
+
+	    String sql = "SELECT * FROM test WHERE school=? AND class_num=? AND subject=? AND num=?";
+	    PreparedStatement statement = connection.prepareStatement(sql);
+	    statement.setString(1, school.getCd());
+	    statement.setInt(2, student.getClassNum().getNum());
+	    statement.setString(3, subject.getCd());
+	    statement.setInt(4, student.getNo());
+
+	    ResultSet resultSet = statement.executeQuery();
+	    while (resultSet.next()) {
+	        Test test = new Test();
+	        test.setId(resultSet.getInt("id"));
+	        test.setSubject(subject);
+	        test.setSchool(school);
+	        test.setNum(resultSet.getInt("num"));
+	        list.add(test);
+	    }
+
+	    return list;
+	}
+
+	private List<Test> postFilter(ResultSet rSet,School school)throws Exception{
+
+		List<Test>list=new ArrayList<>();
+		try{
+			while(rSet.next()){
+				Test test=new Test();
+				test.setNo(rSet.getInt("no"));
+				test.setName(rSet.getString("name"));
+				test.setEntYear(rSet.getInt("ent_year"));
+				test.setClassNum(rSet.getString("class_num"));
+				test.setAttend(rSet.getBoolean("is_attend"));
+				test.setSchool(school);
+				// リストに追加
+				list.add(test);
+			}
+		} catch (SQLException | NullPointerException e) {
+			e.printStackTrace();
+		}
+
+		return list;
+	}
+
+
+
+
+    public List<Test> filter(School school, int classNum, Subject subject, int num) throws Exception {
         List<Test> list = new ArrayList<>();
         Connection connection = getConnection();
 
-        String sql = "SELECT * FROM test WHERE subject_cd = ? AND school_cd = ?";
+        String sql = "SELECT * FROM test WHERE school=? AND class_num=? AND subject=? AND num=?";
         PreparedStatement statement = connection.prepareStatement(sql);
-        statement.setString(1, subject.getCd());
-        statement.setString(2, school.getCd());
+        statement.setString(1, school.getCd());
+        statement.setInt(2, classNum);
+        statement.setString(3, subject.getCd());
+        statement.setInt(4, num);
 
-        ResultSet rset = statement.executeQuery();
-        while (rset.next()) {
+        ResultSet resultSet = statement.executeQuery();
+        while (resultSet.next()) {
             Test test = new Test();
-            test.setNo(rset.getInt("no"));
+            test.setId(resultSet.getInt("id"));
             test.setSubject(subject);
+            test.setNum(resultSet.getInt("num"));
             test.setSchool(school);
-            test.setPoint(rset.getInt("point"));
             list.add(test);
         }
 
-        statement.close();
-        connection.close();
         return list;
     }
 
-    public boolean save(Test test, Connection connection) throws Exception {
+    public boolean saveList(List<Test> list) throws Exception {
+        Connection connection = getConnection();
         boolean result = true;
 
-        String sql = "INSERT INTO test (no, subject_cd, school_cd, point) VALUES (?, ?, ?, ?)";
+        for (Test test : list) {
+            if (!save(test, connection)) {
+                result = false;
+            }
+        }
+
+        return result;
+    }
+
+    public boolean save(Test test, Connection connection) throws Exception {
+        String sql = "INSERT INTO test (subject, num, school) VALUES (?, ?, ?)";
         PreparedStatement statement = connection.prepareStatement(sql);
-        statement.setInt(1, test.getNo());
-        statement.setString(2, test.getSubject().getCd());
-        statement.setString(3, test.getSchool().getCd());
-        statement.setInt(4, test.getPoint());
+        statement.setString(1, test.getSubject().getId());
+        statement.setInt(2, test.getNum());
+        statement.setString(3, test.getSchool().getId());
 
         int rows = statement.executeUpdate();
-        if (rows != 1) result = false;
-
-        statement.close();
-        return result;
+        return rows > 0;
     }
 }
--- src/dao/TestListStudentDao.java
+++ src/dao/TestListStudentDao.java
@@ -1,5 +1,31 @@
 package dao;
 
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+import bean.TestListStudent;
+
 public class TestListStudentDao {
 
+	private List<TestListStudent> postFilte(ResultSet rSet)throws Exceotion{
+		List<TestListStudent>list=new ArrayList<>();
+		try{
+			while(rSet.next()){
+				TestListStudent testliststudent=new TestListStudent();
+				testliststudent.setNo(rSet.getString("no"));
+				testliststudent.setName(rSet.getString("name"));
+				testliststudent.setEntYear(rSet.getInt("ent_year"));
+				testliststudent.setClassNum(rSet.getString("class_num"));
+				testliststudent.setAttend(rSet.getBoolean("is_attend"));
+				list.add(testliststudent);
+			}
+		} catch (SQLException | NullPointerException e) {
+			e.printStackTrace();
+		}
+
+		return list;
+
+	}
 }
