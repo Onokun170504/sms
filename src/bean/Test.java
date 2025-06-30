e3f9bfcbbdabb1ac5c39179ad00ad721f8e787e5
yuuuki05 <ymg2465016@stu.o-hara.ac.jp>
2025-06-30 12:06:13 +0900

    てｓｔ

--- src/bean/Test.java
+++ src/bean/Test.java
@@ -8,50 +8,50 @@ public class Test implements java.io.Serializable{
 	private School school;
 	private int no;
 	private int point;
-	
+
 	public Student getStudent() {
 		return student;
 	}
-	
+
 	public String getClassNum() {
 		return classNum;
 	}
-	
+
 	public Subject getSubject() {
 		return subject;
 	}
-	
+
 	public School getSchool() {
 		return school;
 	}
-	
+
 	public int getNo() {
 		return no;
 	}
-	
+
 	public int getPoint() {
 		return point;
 	}
 	public void setStudent(Student student) {
 		this.student=student;
 	}
-	
+
 	public void setClassNum(String classNum) {
 		this.classNum=classNum;
 	}
-	
+
 	public void setSubject(javax.security.auth.Subject subject2) {
-		this.subject=subject2;
+		this.subject=subject;
 	}
-	
+
 	public void setSchool(School school) {
 		this.school=school;
 	}
-	
+
 	public void setNo(int no) {
 		this.no=no;
 	}
-	
+
 	public void setPoint(int point) {
 		this.point=point;
 	}
